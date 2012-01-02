31e8e9071cd459fa64a10ece1117e14b1f7303dd
Lorance Stinson <LoranceStinson@gmail.com>
2012-01-02 16:54:47 -0500

    Added Perl support.

---
+++ misc/perl_func.pl
@@ -0,0 +1,245 @@
+#!/usr/bin/perl -w
+
+use warnings;
+use strict;
+
+# Generate the Tool Tips for Vim from perlpod -f.
+
+while (my $func = <DATA>) {
+    chomp ($func);
+    my @tool_tip = `perldoc -t -f '$func'`;
+    foreach (@tool_tip) {
+         chomp;
+         s/'/''/g;
+         s/^ {7}//;
+    }
+    $#tool_tip-- unless $tool_tip[-1];
+    if (scalar (@tool_tip) > 30) {
+        $#tool_tip = 29;
+        $#tool_tip-- unless $tool_tip[-2];
+        $tool_tip[-1] = '...';
+    }
+    print "    \\ '$func': [\n";
+    foreach (@tool_tip) {
+         print "    \\   '$_',\n";
+    }
+    print "    \\ ],\n";
+}
+
+__DATA__
+-X
+abs
+accept
+alarm
+atan2
+bind
+binmode
+bless
+caller
+chdir
+chmod
+chomp
+chop
+chown
+chr
+chroot
+close
+closedir
+connect
+continue
+cos
+crypt
+dbmclose
+dbmopen
+defined
+delete
+die
+do
+dump
+each
+endgrent
+endhostent
+endnetent
+endprotoent
+endpwent
+endservent
+eof
+eval
+exec
+exists
+exit
+exp
+fcntl
+fileno
+flock
+fork
+format
+formline
+getc
+getgrent
+getgrgid
+getgrnam
+gethostbyaddr
+gethostbyname
+gethostent
+getlogin
+getnetbyaddr
+getnetbyname
+getnetent
+getpeername
+getpgrp
+getppid
+getpriority
+getprotobyname
+getprotobynumber
+getprotoent
+getpwent
+getpwnam
+getpwuid
+getservbyname
+getservbyport
+getservent
+getsockname
+getsockopt
+glob
+gmtime
+goto
+grep
+hex
+import
+index
+int
+ioctl
+join
+keys
+kill
+last
+lc
+lcfirst
+length
+link
+listen
+local
+localtime
+lock
+log
+lstat
+m
+map
+mkdir
+msgctl
+msgget
+msgrcv
+msgsnd
+my
+next
+no
+package
+prototype
+oct
+open
+opendir
+ord
+our
+pack
+pipe
+pop
+pos
+print
+printf
+push
+q
+qq
+quotemeta
+qw
+qx
+qr
+rand
+read
+readdir
+readline
+readlink
+readpipe
+recv
+redo
+ref
+rename
+require
+reset
+return
+reverse
+rewinddir
+rindex
+rmdir
+s
+scalar
+seek
+seekdir
+select
+semctl
+semget
+semop
+send
+setgrent
+sethostent
+setnetent
+setpgrp
+setpriority
+setprotoent
+setpwent
+setservent
+setsockopt
+shift
+shmctl
+shmget
+shmread
+shmwrite
+shutdown
+sin
+sleep
+socket
+socketpair
+sort
+splice
+split
+sprintf
+sqrt
+srand
+stat
+study
+sub
+substr
+symlink
+syscall
+sysopen
+sysread
+sysseek
+system
+syswrite
+tell
+telldir
+tie
+tied
+time
+times
+tr
+truncate
+uc
+ucfirst
+umask
+undef
+unlink
+unpack
+unshift
+untie
+use
+use 
+utime
+values
+vec
+wait
+waitpid
+wantarray
+warn
+write
+y
